8838cb1b2ba62509a3b7340df8f34118a34e5209
karoliks <karoline.k.sabo@gmail.com>
2020-01-04 13:27:20 +0100

    Add profile picture

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,15 +1,28 @@
 import * as React from "react";
 import ContentConatiner from "src/components/contentContainer/ContentContainer";
 import styled from "styled-components";
+import anonymous from "../../anonymous.svg";
 import { AuthUserContext } from "../../firebase/AuthUserContext";
 import { withAuthorization } from "../../firebase/withAuthorization";
-import slengbukser from "../../Slengbukser farger uten bakgrunn.svg";
 import { PasswordForgetForm } from "../PasswordForget/PasswordForgetForm";
 import { PasswordChangeForm } from "./PasswordChangeForm";
 
-const Image = styled.img.attrs({ src: slengbukser })`
-  height: 400px;
+const Image = styled.img.attrs({ src: anonymous })`
+  /* height: 400px; */
+  height: 200px;
+  width: 200px;
+  background: rgb(232, 240, 254);
+  border-radius: 50%;
+  border-style: solid;
+  border-color: tomato;
+  object-fit: contain;
 `;
+
+// const PicBox = styled.div`
+//   height: 100px;
+//   width: 100px;
+//   background: pink;
+// `;
 
 export const AccountComponent = () => (
   <AuthUserContext.Consumer>
@@ -18,6 +31,7 @@ export const AccountComponent = () => (
         {/* <img src={slengbukser} alt="Slengbukser" /> */}
         <Image />
         <div>
+          <h1>Username: {(authUser as any).username} </h1>
           <h1>Account: {(authUser as any).email}</h1>
           <PasswordForgetForm />
           <PasswordChangeForm />
